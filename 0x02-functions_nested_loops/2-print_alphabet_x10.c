8fc70b17fa9402bfda9ea14ba454089b26167b05
alfaomega77 <oumaimaalfairam@gmail.com>
2023-12-19 12:49:05 -0800

    best

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,25 @@
-#include "putchar.h"
+#include "main.h"
+
 /**
- * print_alphabet_x10 - prints alphabet 10 times
- *
- * Return: always 0;
- *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
-	char i;
-	char h;
+	char ch;
+	int i;
 
-	for (h = 0; h <= 9; h++)
+	i = 0;
+
+	while (i < 10)
 	{
-		for (i = 'a'; i <= 'z'; i++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(i);
+			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
